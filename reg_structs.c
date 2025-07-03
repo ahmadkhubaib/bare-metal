70bc8b117941c14b98b85ed1c1d81f90e2f357c6
Khubaib Ahmad <khubaib.ahmad@visable.com>
2025-07-04 01:16:06 +0200

    files update

--- reg_structs.c
+++ reg_structs.c
@@ -1,35 +1,17 @@
-#include<stdint.h>
-
-#define BASE_PERIPHERAL_ADDR 			(0x40000000UL)
-#define AHB1_PERIPHERAL_OFFSET_ADDR 	(0x00020000UL)
-#define AHB1_BASE_ADDR 					(BASE_PERIPHERAL_ADDR + AHB1_PERIPHERAL_OFFSET_ADDR)
-
-#define GPIO_OFFSET_ADDR 				(0x0000UL)
-#define GPIO_BASE_ADDR 					(AHB1_BASE_ADDR + GPIO_OFFSET_ADDR)
-
-#define RCC_OFFSET 						(0x3800UL)
-#define RCC_BASE						(AHB1_BASE_ADDR + RCC_OFFSET)
-#define RCC_AHB1ENR_OFFSET 				(0x30UL)
-#define RCC_AHB1EN_R					(*(unsigned volatile int *)(RCC_BASE + RCC_AHB1ENR_OFFSET))
-
-#define MODE_R_OFFSET					(0x00UL)
-#define GPIO_MODE_R						(*(unsigned volatile int *)(GPIO_BASE_ADDR + MODE_R_OFFSET))
-
-#define OD_R_OFFSET						(0x14UL)
-#define GPIO_A_OD_R						(*(unsigned volatile int *)(GPIO_BASE_ADDR + OD_R_OFFSET))
-
-
-#define GPIO_A_EN						(1U<<0)
-#define LED_PIN_5						(1U<<5)
-
-// according to MISRA-C standard
-#define __IO volatile
-
 typedef struct {
 	__IO uint32_t MODER;  	/* < GPIO port mode register, 					Address offset: 0x00 	  */
-	uint32_t DUMMY[4];
+	__IO uint32_t OTYPER; 	/* !< GPIO port output type register, 			Address offset: 0x04 	  */
+	__IO uint32_t OSPEEDR;  /* !< GPIO port output speed register, 			Address offset: 0x08 	  */
+	__IO uint32_t PUPDR; 	/* !< GPIO port pull-up/pull-down register, 	Address offset: 0x0C 	  */
+	__IO uint32_t IDR;  	/* !< GPIO port input data register, 			Address offset: 0x10  	  */
 	__IO uint32_t ODR;  	/* < GPIO port output data register, 			Address offset: 0x14 	  */
+	__IO uint32_t BSRR;  	/* < GPIO port bit set/reset register, 			Address offset: 0x18 	  */
+	__IO uint32_t LCKR; 	/* !< GPIO port configuration lock register, 	Address offset: 0x1C 	  */
+	__IO uint32_t AFR[2]; 	/* < GPIO alternate function registers, 		Address offset: 0x20-0x24 */
 }GPIO_TypeDef;
+
+
+
 
 typedef struct {
 	__IO uint32_t CR; /* |< RCC Clock control register, Address offset: 0x00 */
@@ -67,18 +49,3 @@ typedef struct {
 	__IO uint32_t CKGATENR; /* !< RCC Clocks Gating Control Register, Address offset: 0x90 */
 	__IO uint32_t DCKCFGR2; /* !< RCC Dedicated Clocks Configuration Register 2, Address offset: 0x94 */
 } RCC_TypeDef;
-
-int main(void){
-
-	// enable clock access to GPIO A port, OR it with GPIO_A_EN to maintain the value of other bits
-	RCC_AHB1EN_R |= GPIO_A_EN;
-
-	// set port A pin 5 as output
-	GPIO_MODE_R |= (1U<<10 ); //set bit 10 to 1
-	GPIO_MODE_R &=~ (1U<<11); //set bit 11 to 0
-
-	while(1){
-		// set port A pin5 as high
-		GPIO_A_OD_R |= LED_PIN_5;
-	}
-}
